38c39e40c5f5781b6ab8e3fdcd1046e5383a405c
Nick Bild <nick.bild@gmail.com>
2019-11-24 20:02:01 -0500

    Automatically name output files.

--- capture_images.py
+++ capture_images.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 import cv2
 import sys
+import time
 
 
 solenoid_pin = 23 # Pin #16
 try:
-    pitch_number = open("image_count.txt", "r").read().strip()
+    pitch_number = int(open("image_count.txt", "r").read().strip())
 except:
     pitch_number = 0
 
@@ -26,10 +27,11 @@ GPIO.output(solenoid_pin, GPIO.HIGH)
 
 for i in range(2):
     ret, img = cap.read()
-    cv2.imwrite("pitch_{}_{}.jpg".format(pitch_number, i), img)
+    cv2.imwrite("img/pitch_{}_{}.jpg".format(pitch_number, i), img)
 
 GPIO.output(solenoid_pin, GPIO.LOW)
 GPIO.cleanup()
 
 pitch_number += 1
-open("image_count.txt", "w").writepitch_number)
+open("image_count.txt", "w").write(str(pitch_number))
+
