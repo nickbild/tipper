b46d5b51f3ee6f38311146602f75f7ee85bc7b6b
Nick Bild <nick.bild@gmail.com>
2019-11-24 19:53:54 -0500

    Training data capture script.

---
+++ capture_images.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import cv2
+import sys
+
+
+solenoid_pin = 23 # Pin #16
+try:
+    pitch_number = open("image_count.txt", "r").read().strip()
+except:
+    pitch_number = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(solenoid_pin, GPIO.OUT, initial=GPIO.LOW)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# "Warm up" the camera.
+ret, img = cap.read()
+time.sleep(5)
+
+# Launch!
+print("Throwing pitch...")
+GPIO.output(solenoid_pin, GPIO.HIGH)
+
+for i in range(2):
+    ret, img = cap.read()
+    cv2.imwrite("pitch_{}_{}.jpg".format(pitch_number, i), img)
+
+GPIO.output(solenoid_pin, GPIO.LOW)
+GPIO.cleanup()
+
+pitch_number += 1
+open("image_count.txt", "w").writepitch_number)
