a1b124b7fecb425feb6c6d9771edac5ad91071c8
Nick Bild <nick.bild@gmail.com>
2019-11-29 18:08:05 -0500

    Capture frames async to prevent buffering.

--- capture_images.py
+++ capture_images.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import sys
 import time
+import videocaptureasync as vc
 
 
 solenoid_pin = 23 # Pin #16
@@ -14,10 +15,8 @@ except:
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(solenoid_pin, GPIO.OUT, initial=GPIO.LOW)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, 0x47504A4D) # MJPG codec.
-cap.set(3, 640)
-cap.set(4, 480)
+cap = vc.VideoCaptureAsync()
+cap.start()
 
 # "Warm up" the camera.
 ret, img = cap.read()
@@ -27,19 +26,20 @@ time.sleep(5)
 print("Throwing pitch...")
 GPIO.output(solenoid_pin, GPIO.HIGH)
 
-time.sleep(0.220)
+time.sleep(0.140)
 ret, img1 = cap.read()
-time.sleep(0.005)
+time.sleep(0.020)
 ret, img2 = cap.read()
 
-img = np.concatenate((img1, img2), axis=0)
-cv2.imwrite("img/pitch_{}.jpg".format(pitch_number), img)
+img3 = np.concatenate((img1, img2), axis=0)
+cv2.imwrite("img/pitch_{}.jpg".format(pitch_number), img3)
 
 # Save the original images in case the approach needs to change later.
 cv2.imwrite("img/original/pitch_{}_1.jpg".format(pitch_number), img1)
 cv2.imwrite("img/original/pitch_{}_2.jpg".format(pitch_number), img2)
 
 GPIO.output(solenoid_pin, GPIO.LOW)
+cap.stop()
 
 pitch_number += 1
 open("image_count.txt", "w").write(str(pitch_number))
